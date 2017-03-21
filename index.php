8a75b48da928d2c67dad1cb786e15cfc8687e7bd
Hamish B. Lacmane <hamish_5@hotmail.co.uk>
2017-03-21 11:16:07 +0000

    basic template added
    need to clean up and style

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="A Freelance Backend Web Developer looking to develop for you.">
+    <meta name="keywords" content="Web Developer FreeLance HTML PHP JS JQUERY MYSQL">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <title>Hamish B Lacmane - Freelance Web Developer</title>
+    <script src="js/jquery-3.0.0.min.js" type="text/javascript" charset="utf-8"></script>
+  </head>
+  <body>
+    <div id="all">
+      Hamish Lacmane
+      Software Developer
+      Portoflio Re-launch - september 2017
+      <div id="footer">	&copy; Copyright to Hamish Lacmane</div>
+    </div>
+    <!-- End of all -->
+  </body>
+</html>
